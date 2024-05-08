f58cb66649b5fe14a657925080cf31659e801870
challa prathyusha <cprathyusha94@gmail.com>
2024-05-08 00:56:45 -0500

    adding files to the git

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -25,3 +25,17 @@ resource "aws_security_group" "allow_ssh" {
         Created =   "terraform"
     }
 }
+
+#  ec2 instance creation
+resource "aws_instance" "db" {
+    ami =   "ami-090252cbe067a9e58"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = aws_security_group.allow_ssh.id
+
+    tags = {
+      name =  "db"
+      created =   "terraform"
+      environment   =   "dev"
+    }
+
+}
